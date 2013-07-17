f6f97a8c42de8a51bca02ac6481d918937663d05
Abdul Shajin <shajinvk@gmail.com>
2013-07-17 23:17:07 +0530

    ഗണപതി കമിറ്റ്

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magra::Application.config.secret_key_base = '0b88aea439d025686ffc14ce75ef2d1337c757e9daeca4ad5ed06fde7c8bcaa389c2eaf0c36a2ef633753623ff34a5612a704a8d04b19dca9bdbdbb7affcc440'
