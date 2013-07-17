6610ec9a34785449ee1664e1bddd5d0d5d470d89
Abdul Shajin <shajinvk@gmail.com>
2013-07-17 23:24:21 +0530

    ബൂട്ട്സ്ട്രാപ്

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Magra::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the   of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
@@ -39,7 +40,7 @@ Magra::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
